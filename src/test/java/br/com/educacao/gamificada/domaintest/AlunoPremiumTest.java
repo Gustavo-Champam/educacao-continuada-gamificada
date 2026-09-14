f67020a02773afa3c3cac8b3b7eeaecebaba2a21
Gustavo Champam <guti.gustavo10@gmail.com>
2026-09-14 00:36:18 -0300

    test: adiciona cenarios de recompensas premium - red

---
+++ src/test/java/br/com/educacao/gamificada/domaintest/AlunoPremiumTest.java
@@ -0,0 +1,35 @@
+﻿package br.com.educacao.gamificada.domaintest;
+
+import br.com.educacao.gamificada.domain.Aluno;
+import br.com.educacao.gamificada.domain.Plano;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlunoPremiumTest {
+
+    @Test
+    void deveReceberUmVoucherAoVirarPremium() {
+        Aluno aluno = new Aluno("Gustavo", Plano.PREMIUM, 12);
+
+        assertEquals(1, aluno.getVouchers());
+    }
+
+    @Test
+    void deveReceberTresMoedasAoVirarPremium() {
+        Aluno aluno = new Aluno("Gustavo", Plano.PREMIUM, 12);
+
+        assertEquals(3, aluno.getMoedas());
+    }
+
+    @Test
+    void naoDeveReceberRecompensaDuasVezes() {
+        Aluno aluno = new Aluno("Gustavo", Plano.PREMIUM, 12);
+
+        aluno.concederRecompensasPremium();
+        aluno.concederRecompensasPremium();
+
+        assertEquals(1, aluno.getVouchers());
+        assertEquals(3, aluno.getMoedas());
+    }
+}
